cc2b26d543fb9953ede4942b95869614e83dd4e3
xf4c70r <arvindsudarshan18@gmail.com>
2022-12-19 22:46:14 +0530

    New Commit

---
+++ Python/MigratoryBirds.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+
+    id_ar=list(set(arr))
+    id_ar.sort()
+    counter=[]
+    for i in range(len(id_ar)):
+        counter.append(arr.count(id_ar[i]))
+    return(id_ar[counter.index(max(counter))])
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
